db056bd3970d9d73be00a969779a63b6d6a83139
Joaquin Maravillo <joaquin297545@gmail.com>
2021-03-24 18:57:33 -0500

    modify

--- docs/assets/js/Java.js
+++ docs/assets/js/Java.js
@@ -1,15 +1,17 @@
-var searchFormEl = document.querySelector('#search-form');
-function handleSearchFormSubmit(event) {
-  event.preventDefault();
-  var searchInputVal = document.querySelector('#search-input').value;
-  var formatInputVal = document.querySelector('#format-input').value;
-  if (!searchInputVal) {
-    console.error('You need a search input value!');
-    return;
-  }
-  var queryString = /*add next index here/** */ + searchInputVal + '&format=' + formatInputVal;
-  location.replace(queryString);
-}
-searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+// var searchFormEl = document.querySelector('#search-form');
+// function handleSearchFormSubmit(event) {
+//   event.preventDefault();
+//   var searchInputVal = document.querySelector('#search-input').value;
+//   var formatInputVal = document.querySelector('#format-input').value;
+//   if (!searchInputVal) {
+//     console.error('You need a search input value!');
+//     return;
+//   }
+//   var queryString = './bar-listings.html?q=' + searchInputVal + '&format=' + formatInputVal;
+//   location.replace(queryString);
+// }
+// searchFormEl.addEventListener('submit',handleSearchFormSubmit);
 
-// console.log(submit);
+// // console.log(submit);
+
+onclick={("search") => {window.location.href="https://github.com/amandardz/BarScout/blob/main/docs/bar-listings.html"}}

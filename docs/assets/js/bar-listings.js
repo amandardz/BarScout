562b1d343b6892dccf63acf947ea6db4410bca67
Amanda Rodriguez <amandardz15@gmail.com>
2021-03-24 23:03:03 -0500

    modifying files

--- docs/assets/js/bar-listings.js
+++ docs/assets/js/bar-listings.js
@@ -14,6 +14,7 @@ mobileMenuCloseBtn.addEventListener('click', function() {
     
 })
 
+
 var getLocation = function (userInput) {
     var placesUrl = 'https://cors.bridged.cc/https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=' + userInput + '&inputtype=textquery&fields=name,geometry&key=AIzaSyBZy_-Hm-NJBX4uoI3-evIuIKorhTOeQJ8'
 
@@ -26,67 +27,84 @@ var getLocation = function (userInput) {
             cityName.textContent = city.candidates[0].name
 
             var barsUrl = 'https://cors.bridged.cc/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + city.candidates[0].geometry.location.lat + ',' + city.candidates[0].geometry.location.lng + '&radius=1500&type=bar&key=AIzaSyBZy_-Hm-NJBX4uoI3-evIuIKorhTOeQJ8'
-                   
+            
+
+
             fetch(barsUrl)
                 .then(function (response){
                     return response.json();
                 })
                 .then(function (bars){
                     console.log(bars)
-
-                    for(var i = 0; i < bars.results.length; i++){
-                        var barListings = document.querySelector('.bar-listings')
-                        // Creating a container to hold results
-                        var barResultContainer = document.createElement('div')
-                        barResultContainer.classList.add('bar-container')
-                        barListings.append(barResultContainer)
-                        
-                        //Creating a container to hold image within results container
-                        var barImage = document.createElement('div')
-                        barImage.classList.add('bar-image')
-                        barResultContainer.append(barImage)
-                        barImage.innerHTML = '<img src="' + 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=150&photoreference=' + bars.results[i].photos[0].photo_reference + '&key=AIzaSyBZy_-Hm-NJBX4uoI3-evIuIKorhTOeQJ8" alt="bar image">'
-                        
-                        //Creating a container to hold bar information within results container
-                        var barInfo = document.createElement('div')
-                        barInfo.classList.add('bar-info')
-                        barResultContainer.append(barInfo)
-                        barInfo.innerHTML = '<ul class="bar-details">' + '<li>' + 'Bar Name: ' + bars.results[i].name + '</li>' + '<li>' + 'Address: ' + bars.results[i].vicinity+ '</li>' + '</ul>'
-
-
+                    function renderDetails(i) {
                         var getMoreBarInfo = 'https://cors.bridged.cc/https://maps.googleapis.com/maps/api/place/details/json?place_id=' + bars.results[i].place_id + '&fields=formatted_phone_number,opening_hours,website&key=AIzaSyBZy_-Hm-NJBX4uoI3-evIuIKorhTOeQJ8' 
-
+        
                         fetch(getMoreBarInfo)
                             .then(function(response){
                                 return response.json();
                             })
                             .then(function (info) {
                                 console.log(info)
-                                for(i = 0; i < bars.results[i].length; i++)
-                                var barInfoEl = document.querySelector('.bar-info')
-                                var phoneNum = document.createElement('div')
-                                barInfoEl.append(phoneNum)
-                                phoneNum.textContent = 'Phone Number: ' + info.result.formatted_phone_number
+                                var barListings = document.querySelector('.bar-listings')
+                                // Creating a container to hold results
+                                var barResultContainer = document.createElement('div')
+                                barResultContainer.classList.add('bar-container')
+                                barListings.append(barResultContainer)
+                                
+                                //Creating a container to hold image within results container
+                                var barImage = document.createElement('div')
+                                barImage.classList.add('bar-image')
+                                barResultContainer.append(barImage)
+                                barImage.innerHTML = '<img class="image-style" src="' + 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=300&maxheight=300&photoreference=' + bars.results[i].photos[0].photo_reference + '&key=AIzaSyBZy_-Hm-NJBX4uoI3-evIuIKorhTOeQJ8" alt="bar image">'
+                                
+                                //Creating a container to hold bar information within results container
+                                var barInfo = document.createElement('div')
+                                barInfo.classList.add('bar-info')
+                                barResultContainer.append(barInfo)
+                                barInfo.innerHTML = '<ul class="bar-details">' + '<li>' + 'Bar Name: ' + bars.results[i].name + '</li>' + '<li>' + 'Address: ' + bars.results[i].vicinity+ '</li>' + '</ul>'
+                                
+                                if(info.result.formatted_phone_number) {
+                                    var phoneNum = document.createElement('div')
+                                    barInfo.append(phoneNum)
+                                    console.log(phoneNum)
+                                    phoneNum.textContent = 'Phone Number: ' + info.result.formatted_phone_number
+                                }
+                                
+
                             })
                     }
+                    for(var i = 0; i < bars.results.length; i++){
+                        renderDetails(i)
 
-                    // function initMap() {
-                    //     var location = {lat: city.candidates[0].geometry.location.lat, lng: city.candidates[0].geometry.location.lng};
-                    //     var map = new google.maps.Map(document.getElementById('map'), {
-                    //         zoom: 15,
-                    //         center: location
-                    //     });
+                        new google.maps.Marker({
+                            map,
+                            title: bars.results[i].name,
+                            position: bars.results[i].geometry.location
+                          });
+                    }
+
+                        var location = {lat: city.candidates[0].geometry.location.lat, lng: city.candidates[0].geometry.location.lng};
+
+                        map.panTo(location)
                         
-                    // }
+
                 })
+            
         })
 };
-
-
+var map;
+var mapEl = document.getElementById('maps')
+function initMap () {
+    map = new google.maps.Map(mapEl, {
+        zoom: 15,
+    });
+}
 
 searchBtn.addEventListener('click', function(event){
     event.preventDefault();
     var userInput = searchInput.value
     console.log(userInput);
     getLocation(userInput);
+    mapEl.style.display = 'block';
+    
 });

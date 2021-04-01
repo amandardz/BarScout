6268754f7b6dee1ce8b4bc0a82fc6d8457c13588
Nate Worley <immythz@gmail.com>
2021-04-01 18:44:07 -0500

    Added local storage

--- docs/assets/js/bar-listings.js
+++ docs/assets/js/bar-listings.js
@@ -8,21 +8,42 @@ var barListings = document.querySelector('.bar-listings');
 var homeBtn = document.querySelector('.btn');
 
 // Event Listeners for Hamburger Menu
-mobileMenuBtn.addEventListener('click', function() {
+mobileMenuBtn.addEventListener('click', function () {
     menuLinks.classList.add('active');
 });
 
-mobileMenuCloseBtn.addEventListener('click', function() {
+mobileMenuCloseBtn.addEventListener('click', function () {
     menuLinks.classList.remove('active');
-    
+
 })
+
+// Local Storage For Saved Searches
+function save() {
+    var new_data = ' ' + document.getElementById('results').value;
+
+    if (localStorage.getItem('data') == null) {
+        localStorage.setItem('data', '[]');
+    }
+
+    var old_data = JSON.parse(localStorage.getItem('data'));
+    old_data.push(new_data);
+
+    localStorage.setItem('data', JSON.stringify(old_data));
+}
+
+function view() {
+    if (localStorage.getItem('data') != null) {
+        document.getElementById('output').innerHTML = JSON.parse(localStorage.getItem('data'));
+    }
+}
+
 
 // Retrieving bar information according to user criteria from Google API
 var getLocation = function (userInput) {
     var placesUrl = 'https://cors.bridged.cc/https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=' + userInput + '&inputtype=textquery&fields=name,geometry&key=AIzaSyBZy_-Hm-NJBX4uoI3-evIuIKorhTOeQJ8'
 
     fetch(placesUrl)
-        .then(function (response){
+        .then(function (response) {
             return response.json();
         })
         .then(function (city) {
@@ -30,21 +51,21 @@ var getLocation = function (userInput) {
             cityName.textContent = city.candidates[0].name
 
             var barsUrl = 'https://cors.bridged.cc/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + city.candidates[0].geometry.location.lat + ',' + city.candidates[0].geometry.location.lng + '&radius=1500&type=bar&key=AIzaSyBZy_-Hm-NJBX4uoI3-evIuIKorhTOeQJ8'
-            
+
 
 
             fetch(barsUrl)
-                .then(function (response){
+                .then(function (response) {
                     return response.json();
                 })
-                .then(function (bars){
+                .then(function (bars) {
                     console.log(bars)
 
                     function renderDetails(i) {
-                        var getMoreBarInfo = 'https://cors.bridged.cc/https://maps.googleapis.com/maps/api/place/details/json?place_id=' + bars.results[i].place_id + '&fields=formatted_phone_number,opening_hours,website&key=AIzaSyBZy_-Hm-NJBX4uoI3-evIuIKorhTOeQJ8' 
-        
+                        var getMoreBarInfo = 'https://cors.bridged.cc/https://maps.googleapis.com/maps/api/place/details/json?place_id=' + bars.results[i].place_id + '&fields=formatted_phone_number,opening_hours,website&key=AIzaSyBZy_-Hm-NJBX4uoI3-evIuIKorhTOeQJ8'
+
                         fetch(getMoreBarInfo)
-                            .then(function(response){
+                            .then(function (response) {
                                 return response.json();
                             })
                             .then(function (info) {
@@ -54,34 +75,34 @@ var getLocation = function (userInput) {
                                 var barResultContainer = document.createElement('div')
                                 barResultContainer.classList.add('bar-container')
                                 barListings.append(barResultContainer)
-                                
+
                                 //Creating a container to hold bar image within results container
                                 var barImage = document.createElement('div')
                                 barImage.classList.add('bar-image')
                                 barResultContainer.append(barImage)
                                 barImage.innerHTML = '<img class="image-style" src="' + 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=300&maxheight=300&photoreference=' + bars.results[i].photos[0].photo_reference + '&key=AIzaSyBZy_-Hm-NJBX4uoI3-evIuIKorhTOeQJ8" alt="bar image">'
 
-                                
+
                                 //Creating a container to hold bar information within results container
                                 var barInfo = document.createElement('div')
                                 barInfo.classList.add('bar-info')
                                 barResultContainer.append(barInfo)
                                 barInfo.innerHTML = '<ul class="bar-details">' + '<li>' + 'Bar Name: ' + bars.results[i].name + '</li>' + '<li>' + 'Address: ' + bars.results[i].vicinity + '</li>' + '</ul>'
 
-                                if(info.result.formatted_phone_number) {
+                                if (info.result.formatted_phone_number) {
                                     var phoneNum = document.createElement('li')
                                     phoneNum.classList.add('bar-phone')
                                     barInfo.append(phoneNum)
-                                    phoneNum.innerHTML = 'Phone Number: ' + '<a href="tel:' + info.result.formatted_phone_number + '">' + info.result.formatted_phone_number + '</a>'
+                                    phoneNum.innerHTML = 'Phone Number: ' + '<a href="tel:' + info.result.formatted_phone_number + '">' + info.result.formatted_phone_number + '</a>'
                                 } else {
                                     var phoneNum = document.createElement('li')
                                     phoneNum.classList.add('bar-phone')
                                     barInfo.append(phoneNum)
                                     phoneNum.innerHTML = 'Phone Number: UNAVAILABLE'
                                 }
-                                
+
                                 //Event Listener to move down to the map when user clicks on the bar result container
-                                barResultContainer.addEventListener('click', function(){
+                                barResultContainer.addEventListener('click', function () {
                                     document.getElementById('map').scrollIntoView({
                                         behavior: 'smooth'
                                     });
@@ -90,39 +111,42 @@ var getLocation = function (userInput) {
                     }
 
                     //Creating Map with markers according to user inputted criteria and the surrounding bars to that location
-                    for(var i = 0; i < bars.results.length; i++){
+                    for (var i = 0; i < bars.results.length; i++) {
                         renderDetails(i)
 
                         new google.maps.Marker({
                             map,
                             title: bars.results[i].name,
                             position: bars.results[i].geometry.location
-                          });
+                        });
                     }
 
-                        var location = {lat: city.candidates[0].geometry.location.lat, lng: city.candidates[0].geometry.location.lng};
+                    var location = {
+                        lat: city.candidates[0].geometry.location.lat,
+                        lng: city.candidates[0].geometry.location.lng
+                    };
 
-                        map.panTo(location)
-                        
+                    map.panTo(location)
+
 
 
                 })
-            
+
         })
-    
+
 };
 
 var map;
 var mapEl = document.getElementById('map')
 
-function initMap () {
+function initMap() {
     map = new google.maps.Map(mapEl, {
         zoom: 15,
     });
 }
 
 
-searchBtn.addEventListener('click', function(event){
+searchBtn.addEventListener('click', function (event) {
     event.preventDefault();
     var userInput = searchInput.value
     console.log(userInput);
